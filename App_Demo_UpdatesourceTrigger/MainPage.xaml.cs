211bd36c93eb4c6b7812df44ab01c545ae8d4123
trungngotdt <trungngotdt@outlook.com.vn>
2017-06-14 15:22:37 +0700

    First commit

---
+++ App_Demo_UpdatesourceTrigger/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App_Demo_UpdatesourceTrigger
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page,INotifyPropertyChanged
+    {
+        // create a object to save data
+        private string data;
+        public string Data { get => data; set { data = value;OnPropertyChanged("Data"); } }
+        //----------------------
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Data = "A";
+            this.DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        internal void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
